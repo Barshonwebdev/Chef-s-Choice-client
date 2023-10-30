64bafdd7c9cbf544751c21046b701726419e36b1
Barshon <barshonweb@gmail.com>
2023-10-30 21:31:43 +0600

    navbar design responsive

--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import './Header.css'
 import { NavLink } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 const Header = () => {
     return (
-      <div className="d-flex mt-5 text-md-start text-center align-items-center">
-        <div className="me-5">
+      <div className="d-flex flex-column flex-md-row mt-5 text-md-start text-center align-items-center">
+        <div className="me-md-5">
           <h2 className="fw-bolder">Chef`s Choice</h2>
           <small className="title-text text-uppercase">
             Ultimate cookbook of chefs
           </small>
         </div>
-        <div className="ms-5  ">
-          <NavLink to='/' className="me-5 navs text-decoration-none">Home</NavLink>
-          <NavLink to='/about' className="me-5 navs text-decoration-none">About us</NavLink>
-          <NavLink to='/categories' className="me-5 navs text-decoration-none">Categories</NavLink>
-          <NavLink to='/blogs' className="me-5 navs text-decoration-none">Blogs</NavLink>
-        </div>
-        
+        <Navbar expand="lg ms-md-5 ">
+          <Navbar.Toggle className="mx-auto" aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mt-2">
+              <NavLink to="/" className="me-md-5 navs text-decoration-none">
+                Home
+              </NavLink>
+              <NavLink
+                to="/about"
+                className="me-md-5 navs text-decoration-none"
+              >
+                About us
+              </NavLink>
+              <NavLink
+                to="/categories"
+                className="me-md-5 navs text-decoration-none"
+              >
+                Categories
+              </NavLink>
+              <NavLink
+                to="/blogs"
+                className="me-md-5 navs text-decoration-none"
+              >
+                Blogs
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
 };
