119b7fc6e993c5013b15dc66ccbc9e6afebc0d9d
Barshon <barshonweb@gmail.com>
2023-10-30 13:46:13 +0600

    title and headline

--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 import { Container } from 'react-bootstrap';
 const Header = () => {
     return (
-        <Container className='mt-3'>
+        <Container className='mt-5'>
             <h2 className='fw-bolder'>Chef`s Choice</h2>
             <small className='title-text text-uppercase'>Ultimate cookbook of chefs</small>
         </Container>
