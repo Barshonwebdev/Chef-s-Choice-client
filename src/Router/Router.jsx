9184a7b065ec94ced792ab49dd43e9433f529a8e
Barshon <barshonweb@gmail.com>
2023-10-30 21:02:30 +0600

    navbar design

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home";
+import Blogs from "../pages/Blogs/Blogs";
+import Categories from "../pages/Categories/Categories";
+import About from "../pages/About/About";
 
 const router = createBrowserRouter([
   {
@@ -10,7 +13,19 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>
-      }
+      },
+      {
+        path: "/blogs",
+        element: <Blogs></Blogs>
+      },
+      {
+        path: "/categories",
+        element: <Categories></Categories>
+      },
+      {
+        path: "/about",
+        element: <About></About>
+      },
     ],
   },
 ]);
--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './Header.css'
-import { Container } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 const Header = () => {
     return (
-        <div className='mt-5 text-md-start text-center'>
-            <h2 className='fw-bolder'>Chef`s Choice</h2>
-            <small className='title-text text-uppercase'>Ultimate cookbook of chefs</small>
+      <div className="d-flex mt-5 text-md-start text-center align-items-center">
+        <div className="me-5">
+          <h2 className="fw-bolder">Chef`s Choice</h2>
+          <small className="title-text text-uppercase">
+            Ultimate cookbook of chefs
+          </small>
         </div>
+        <div className="ms-5  ">
+          <NavLink to='/' className="me-5 navs text-decoration-none">Home</NavLink>
+          <NavLink to='/about' className="me-5 navs text-decoration-none">About us</NavLink>
+          <NavLink to='/categories' className="me-5 navs text-decoration-none">Categories</NavLink>
+          <NavLink to='/blogs' className="me-5 navs text-decoration-none">Blogs</NavLink>
+        </div>
+        
+      </div>
     );
 };
 
