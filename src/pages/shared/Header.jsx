c5202e49cdac5c5d3cd8a55bbe145ffe7e120d1d
Barshon <barshonweb@gmail.com>
2023-10-30 12:58:14 +0600

    title and headline

---
+++ src/pages/shared/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <p>header</p>
+            <h2 className='fw-bolder'>Chef`s Choice</h2>
+        </div>
+    );
+};
+
+export default Header;
