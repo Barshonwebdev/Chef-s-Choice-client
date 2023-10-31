e8feb0177c3d40600e12277d0d5136c9bddd1d5b
Barshon <barshonweb@gmail.com>
2023-10-31 13:14:39 +0600

    footer created (responsive)

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
       
         <div className="bg justify-content-center d-flex flex-column align-items-center">
-            <h2 className="fw-bolder">Chef`s Choice</h2>
+            <h1 className="fw-bolder title ">Chef`s Choice</h1>
             <small className="fw-bolder text-uppercase">
               Ultimate Japanese cookbook of chefs </small>
               <small className='copyright'>2023, Chef`s Choice. All rights reserved</small>
--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <Container>
         <div className="d-flex flex-column flex-md-row mt-5 text-md-start text-center align-items-center">
           <div className="me-md-5">
-            <h2 className="fw-bolder">Chef`s Choice</h2>
+            <h2 className="fw-bolder title">Chef`s Choice</h2>
             <small className="title-text text-uppercase">
               Ultimate Japanese cookbook of chefs
             </small>
