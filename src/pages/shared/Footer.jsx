eadf97b7785d9212ff3dc8ebb4ae29cad4a0ef74
Barshon <barshonweb@gmail.com>
2023-10-31 19:25:59 +0600

    footer correction

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import './Footer.css'
 const Footer = () => {
     return (
-      
-        <div className="bg justify-content-center d-flex flex-column align-items-center">
-            <h1 className="fw-bolder title ">Chef`s Choice</h1>
-            <small className="fw-bolder text-uppercase">
-              Ultimate Japanese cookbook of chefs </small>
-              <small className='copyright '>2023, Chef`s Choice. All rights reserved</small>
+      <div className="bg justify-content-center d-flex flex-column align-items-center">
+        <div className=" boxed d-flex flex-column align-items-center">
+          <h1 className="fw-bolder title ">Chef`s Choice</h1>
+          <small className="fw-bolder text-uppercase">
+            Ultimate Japanese cookbook of chefs{" "}
+          </small>
         </div>
-      
+        <small className="copyright ">
+          2023, Chef`s Choice. All rights reserved
+        </small>
+      </div>
     );
 };
 
