ad35d42f9386c12ff13431c0af2004455af3b266
Barshon <barshonweb@gmail.com>
2023-11-01 18:18:18 +0600

    about page created (responsive)

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import './About.css'
 const About = () => {
     return (
-        <div>
-            
+      <div>
+        <div className="mt-5">
+          <div className="d-flex justify-content-center">
+            <img
+              className="rounded rounded-xl img-fluid "
+              src="../../../about.jpg"
+              alt=""
+            />
+          </div>
+          <p className='fs-3 mt-5 w-md-50 mx-4 title fw-bold text-secondary'>
+            Our site represents perfectly what it means to be a cookbook for the
+            aspiring chefs practicing Japanese Cuisines. We provide authentic
+            recipes from renowned chefs and make sure that the learning curve do
+            not become steep. For further info, visit our recipes!
+          </p>
         </div>
+      </div>
     );
 };
 
