61425689df34fc7add42f6c2e0502a4b08ff8902
Barshon <barshonweb@gmail.com>
2023-11-02 00:17:20 +0600

    404 corrections

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2 className='mt-5 text-center text-danger'>404! Page not found!</h2>
-            <div className='mt-2 mb-5 d-flex justify-content-center '>
-                <img className='img-fluid w-50 rounded rounded-lg' src="../../../not-found.jpg" alt="" />
-            </div>
+      <div>
+        <div className="mt-4 d-flex justify-content-center align-items-center">
+          <h2 className=" text-danger me-4">404! Page not found!</h2>
+          <Link to='/'>
+            <button className="btn btn-primary">Go back</button>
+          </Link>
         </div>
+        <div className="mt-2 mb-5 d-flex justify-content-center ">
+          <img
+            className="img-fluid w-50 rounded rounded-lg"
+            src="../../../not-found.jpg"
+            alt=""
+          />
+        </div>
+      </div>
     );
 };
 
