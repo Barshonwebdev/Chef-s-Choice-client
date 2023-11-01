da982a384967d15b49e96ee616cec4d133a14365
Barshon <barshonweb@gmail.com>
2023-11-01 21:06:05 +0600

    categories page

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
-
+import './categories.css'
+import { Col, Container, Row } from 'react-bootstrap';
+import { BiBowlRice } from "react-icons/bi";
+import { TbMeat } from "react-icons/tb";
+import { PiFish } from "react-icons/pi";
+import { LuSalad } from "react-icons/lu";
+import { LuCakeSlice } from "react-icons/lu";
+import { BiDrink } from "react-icons/bi";
 const Categories = () => {
     return (
       <div>
-        <div className='mt-5'>
+        <div className="mt-5 mb-5">
           <p className="text-center fs-2">Our Recipe Categories</p>
+          <Container>
+            <Row className="">
+              <Col
+                className=" d-flex justify-content-end align-items-center "
+                xs={6}
+              >
+                <h3 className="icon me-2">Rice</h3>
+                <BiBowlRice className="icon fs-1"></BiBowlRice>
+              </Col>
+              <Col
+                className=" d-flex justify-content-start align-items-center "
+                xs={6}
+              >
+                <h3 className="icon me-2">Meat</h3>
+                <TbMeat className="icon fs-1"></TbMeat>
+              </Col>
+            </Row>
+            <Row className="">
+              <Col
+                className=" d-flex justify-content-end align-items-center "
+                xs={6}
+              >
+                <h3 className="icon me-2">Fish</h3>
+                <PiFish className="icon fs-1"></PiFish>
+              </Col>
+              <Col
+                className=" d-flex justify-content-start align-items-center "
+                xs={6}
+              >
+                <h3 className="icon me-2">Salad</h3>
+                <LuSalad className="icon fs-1"></LuSalad>
+              </Col>
+            </Row>
+            <Row className="">
+              <Col
+                className=" d-flex justify-content-end align-items-center "
+                xs={6}
+              >
+                <h3 className="icon me-2">Dessert</h3>
+                <LuCakeSlice className="icon fs-1"></LuCakeSlice>
+              </Col>
+              <Col
+                className=" d-flex justify-content-start align-items-center "
+                xs={6}
+              >
+                <h3 className="icon me-2">Drinks</h3>
+                <BiDrink className="icon fs-1"></BiDrink>
+              </Col>
+            </Row>
+          </Container>
         </div>
       </div>
     );
