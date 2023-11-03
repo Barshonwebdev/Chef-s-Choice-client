e366986be6ddd9d1ec4490b2a8ce62d067fcb6ed
Barshon <barshonweb@gmail.com>
2023-11-03 13:00:49 +0600

    chef names shown in UI

--- src/pages/Home/Chefs/EachChef/EachChef.jsx
+++ src/pages/Home/Chefs/EachChef/EachChef.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
 
-const EachChef = () => {
+const EachChef = ({chef}) => {
+  const {chef_name}= chef;
     return (
       <div>
         <Col>
           <Card>
             <Card.Img variant="top" src="holder.js/100px160" />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
+              <Card.Title>{chef_name}</Card.Title>
               <Card.Text>
                 This is a longer card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
