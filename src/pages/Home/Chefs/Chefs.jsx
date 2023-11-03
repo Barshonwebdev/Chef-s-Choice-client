8501586a9acf53fd53ff98fbf5f043914d0d0dc9
Barshon <barshonweb@gmail.com>
2023-11-03 12:58:32 +0600

    chef cards generated dynamically (data from server loaded)

--- src/pages/Home/Chefs/Chefs.jsx
+++ src/pages/Home/Chefs/Chefs.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
+import EachChef from './EachChef/EachChef';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Chefs = () => {
+  const [chefs,setChefs]=useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:5000/chefs")
+    .then(res=>res.json())
+    .then(data=>setChefs(data))
+  },[])
     return (
       <div>
         <Container>
@@ -9,7 +18,9 @@ const Chefs = () => {
             <h4 className="headline mb-5">Our Renowned Chefs</h4>
             <div>
               <Row xs={1} md={3} className="g-4">
-
+                {
+                  chefs.map(chef=><EachChef key={chef.id}></EachChef>)
+                }
               </Row>
             </div>
           </div>
