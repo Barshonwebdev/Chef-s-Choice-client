2a871c1207d5b13dc9e3abab99de80771cdb017f
Barshon <barshonweb@gmail.com>
2023-11-03 16:44:25 +0600

    view recipe button route setup (start)

--- src/pages/Home/Chefs/EachChef/EachChef.jsx
+++ src/pages/Home/Chefs/EachChef/EachChef.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Card, Col } from 'react-bootstrap';
 import './EachChef.css'
 import { AiOutlineLike } from "react-icons/ai";
+import { NavLink } from "react-router-dom";
 
 const EachChef = ({chef}) => {
   const {chef_name,chef_picture,years_of_experience,number_of_recipes,likes}= chef;
@@ -11,13 +12,20 @@ const EachChef = ({chef}) => {
           <Card>
             <Card.Img variant="top" src={chef_picture} />
             <Card.Body>
-              <Card.Title className='chef-name'>{chef_name}</Card.Title>
+              <Card.Title className="chef-name">{chef_name}</Card.Title>
               <Card.Text>
-                <p className='text-color'>Experience: {years_of_experience} years</p>
-                <p className='text-color'>Total recipes: {number_of_recipes}</p>
-                <p className='text-color d-flex align-items-center'><AiOutlineLike className='fs-2 text-color'></AiOutlineLike> <span className='fs-5'>{likes}</span></p>
+                <p className="text-color">
+                  Experience: {years_of_experience} years
+                </p>
+                <p className="text-color">Total recipes: {number_of_recipes}</p>
+                <p className="text-color d-flex align-items-center">
+                  <AiOutlineLike className="fs-2 text-color"></AiOutlineLike>{" "}
+                  <span className="fs-5">{likes}</span>
+                </p>
               </Card.Text>
-              <Button variant='secondary'>View Recipes</Button>
+              <NavLink to='/categories'> 
+                <Button variant="secondary">View Recipes</Button>{" "}
+              </NavLink>
             </Card.Body>
           </Card>
         </Col>
