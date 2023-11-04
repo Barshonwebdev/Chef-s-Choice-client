b47d313112286f7c9b4143bf6c2f97331f2ca9d1
Barshon <barshonweb@gmail.com>
2023-11-04 15:56:55 +0600

    Firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCk6QqrHGnbUcp7DrPUTsOQ4nkbpTAKHts",
+  authDomain: "chef-s-choice-e11f5.firebaseapp.com",
+  projectId: "chef-s-choice-e11f5",
+  storageBucket: "chef-s-choice-e11f5.appspot.com",
+  messagingSenderId: "250366463419",
+  appId: "1:250366463419:web:d2b899772a71405ae8c89e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
