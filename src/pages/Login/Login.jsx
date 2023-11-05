9fdfdcd4b44e5df3c045d7645c5a9a2331dbe002
Barshon <barshonweb@gmail.com>
2023-11-05 22:55:13 +0600

    google signin implemented

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import { useContext } from 'react';
 import './Login.css'
-import { Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider';
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebookF } from "react-icons/fa";
+
+
 const Login = () => {
+  const {googleSignIn}=useContext(AuthContext);
+  const signInwithGoogle=()=>{
+    googleSignIn()
+    .then(result=>{
+      const loggedInUser=result.user;
+      console.log(loggedInUser);
+    })
+  }
     return (
       <div className="d-flex justify-content-center align-items-center mt-5 mb-5">
         <Card className="p-5 bg-dark text-light">
@@ -30,12 +43,32 @@ const Login = () => {
                 placeholder="enter your password "
               />
             </div>
-            <div className='d-flex justify-content-center'>
+            <div className="d-flex justify-content-center">
               <button type="submit" className="btn btn-secondary ">
                 Login
               </button>
             </div>
-            <p className='text-center mt-3'>New here? <Link className='text-color text-decoration-none' to='/register'>Register!</Link></p>
+            <p className="text-center mt-3">
+              New here?{" "}
+              <Link className="text-color text-decoration-none" to="/register">
+                Register!
+              </Link>
+            </p>
+            <p className="text-center">Or...</p>
+            <Button
+              variant="light"
+              className="zoom me-2"
+              onClick={signInwithGoogle}
+            >
+              <FcGoogle className='fs-4 me-1'></FcGoogle>Sign In
+            </Button>
+            <Button
+              variant="light"
+              className="zoom"
+              onClick={signInwithGoogle}
+            >
+              <FaFacebookF className='fs-5 me-1 text-primary'></FaFacebookF>Log In
+            </Button>
           </form>
         </Card>
       </div>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import getAuth from "firebase/auth";
+import React, { createContext, useState } from 'react';
+import  { getAuth,GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 
 const auth=getAuth(app);
+const provider= new GoogleAuthProvider();
+export  const AuthContext= createContext(null);
 
 
+const AuthProvider = ({children}) => {
+const  [user,setUser]=useState(null);
 
-const AuthProvider = () => {
+const [loading,setLoading]=useState(true);
 
+const googleSignIn=()=>{
+    setLoading(true);
+    return signInWithPopup(auth,provider);
+}
+const authInfo={user,googleSignIn,loading}
     
     return (
-        
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
