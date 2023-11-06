79af70d7c1d8902b28139971ede84c1a50a55c8e
Barshon <barshonweb@gmail.com>
2023-11-06 18:51:17 +0600

    private route implemented

--- src/privateRoutes/PrivateRoutes.jsx
+++ src/privateRoutes/PrivateRoutes.jsx
@@ -6,7 +6,7 @@ const PrivateRoutes = ({children}) => {
     const {user,loading}=useContext(AuthContext);
 
     if(loading){
-        return <small>...</small>
+        return <small>Loading...</small>
     }
     if (user) {
       return children;
